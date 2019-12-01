e641c58af952830ab55ac8134f90d34fe2105bba
JaimeRamos99 <jrossetes@uninorte.edu.co>
2019-12-01 01:25:49 -0500

    bienvenido

--- src/components/bienvenido.js
+++ src/components/bienvenido.js
@@ -3,7 +3,7 @@ export default class Bienvenido extends React.Component {
     render() {
         return (
             <div style={{ backgroundColor: '#22264b' }}>
-                <div style={{ display: 'flex',paddingTop:100, justifyContent: 'center', height: '100vh', width: '100vw', backgroundImage: 'url(https://storage.googleapis.com/primersegmentoholu/HomePRO.png)', backgroundRepeat: 'no-repeat', backgroundSize: 'contain' }}>
+                <div style={{ display: 'flex', paddingTop: 100, justifyContent: 'center', height: '100vh', width: '100vw', backgroundImage: 'url(https://storage.googleapis.com/primersegmentoholu/HomePRO.png)', backgroundRepeat: 'no-repeat', backgroundSize: 'contain' }}>
                     <h2>¡Ya eres parte de Nearby!</h2>
                 </div>
             </div>
